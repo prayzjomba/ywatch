3c1911e477900ec04fe858b572131435d1c12530
Prayz Jomba <prayzjomba@protonmail.com>
2021-10-25 13:07:29 +0300

    scaled window

--- ywatch/yw.py
+++ ywatch/yw.py
@@ -236,7 +236,7 @@ if file.endswith('ts'):
 
 #OPEN
 if userIn.watch:
-    subprocess.Popen(['mpv', fs, file, '--loop-playlist', '--player-operation-mode=pseudo-gui', '--autofit-larger=30%'])
+    subprocess.Popen(['mpv', fs, file, '--loop-playlist', '--player-operation-mode=pseudo-gui', '--autofit-larger=70%x70%'])
 
 
 exit()
